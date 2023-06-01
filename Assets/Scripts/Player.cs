ea7161da5910e0911c87b0e9c86b99262f664178
XEXYDESIGNS <47199015+XEXYDESIGNS@users.noreply.github.com>
2023-06-01 14:12:42 -0400

    Adding some new features to player moment and shield strength

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject _tripleShotPrefab;
     [SerializeField] private GameObject _speedPrefab;
     [SerializeField] private GameObject _shieldsPrefab;
+    [SerializeField] private int _shieldHits = 3;
     
     [SerializeField] private GameObject _visualShields;
     
@@ -98,10 +99,22 @@ public class Player : MonoBehaviour
     {
         float horizonalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
-        
 
-        Vector3 direction = new Vector3(horizonalMovement, verticalMovement, 0);
-        transform.Translate(direction * _speed * Time.deltaTime);
+        float leftArrowSpeed = 3.0f;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _speed = _speed * leftArrowSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _speed = _speed / leftArrowSpeed;
+        }
+        else
+        {
+            Vector3 direction = new Vector3(horizonalMovement, verticalMovement, 0);
+            transform.Translate(direction * _speed * Time.deltaTime);
+        }
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minHeight, maxHeight), 0);
 
@@ -134,31 +147,46 @@ public class Player : MonoBehaviour
 
     public void Damage()
     {
-        if (_isShieldsActive == true)
+        if (_isShieldsActive == true && _shieldHits == 3)
+        {
+            //lasts for 3 hits, change colour each hit.
+            GameObject.Find("Shields").GetComponent<Renderer>().material.color = Color.green;
+            _shieldHits--;
+        }
+        else if (_isShieldsActive == true && _shieldHits == 2)
+        {
+            GameObject.Find("Shields").GetComponent<Renderer>().material.color = Color.magenta;
+            _shieldHits--;
+        }
+        else if (_isShieldsActive == true && _shieldHits == 1)
         {
             _isShieldsActive = false;
             _visualShields.SetActive(false);
+            _shieldHits = 3;
             return;
         }
 
-        _lives--;
+        if (_isShieldsActive == false)
+        {
+            _lives--;
 
-        if (_lives == 2)
-        {
-            _rightEngine.SetActive(true);
-        }
-        else if (_lives == 1)
-        {
-            _leftEngine.SetActive(true);
-        }
-        
-        _uiManager.UpdateLives(_lives);
+            if (_lives == 2)
+            {
+                _rightEngine.SetActive(true);
+            }
+            else if (_lives == 1)
+            {
+                _leftEngine.SetActive(true);
+            }
 
-        if (_lives < 1)
-        {
-            _spawnManager.OnPlayerDeath();
-            Destroy(this.gameObject);
-            _explosionSound.Play();
+            _uiManager.UpdateLives(_lives);
+
+            if (_lives < 1)
+            {
+                _spawnManager.OnPlayerDeath();
+                Destroy(this.gameObject);
+                _explosionSound.Play();
+            }
         }
     }
 
